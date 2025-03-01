b8f96f38679676aaf9a1f71a162f8e69ba447488
Abhishek Kumar <abhirajabhi312@gmail.com>
2025-03-02 00:33:11 +0530

    added functions to start and update WAV header data

--- pkg/audio/recorder.go
+++ pkg/audio/recorder.go
@@ -2,9 +2,11 @@ package audio
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/gordonklaus/portaudio"
 )
@@ -60,5 +62,80 @@ func NewRecorder(filename string) (*Recorder, error) {
 		return nil, fmt.Errorf("failed to open audio stream: ", err)
 
 	}
+	rec.stream = stream
+	return rec, nil
 
+}
+
+// Start --> initiate recording of audio from source with specified duration (5s)
+func (r *Recorder) Start(duration time.Duration) error {
+	if r.stream == nil {
+		return errors.New("audio stream not initialized ")
+	}
+
+	slog.Info("Recording started", "duration", duration)
+
+	if err := r.stream.Start(); err != nil {
+		return fmt.Errorf("failed to start recording: %w", err)
+	}
+
+	time.Sleep(duration)
+
+	if err := r.stream.Stop(); err != nil {
+		return fmt.Errorf("failed to stop recording: %w", err)
+	}
+
+	slog.Info("Recording finish")
+	return nil
+}
+
+// Stop --> closesand relases the resources
+func (r *Recorder) Close() {
+	if r.stream != nil {
+		r.stream.Close()
+	}
+	if r.file != nil {
+		updateWAVHeader(r.file)
+		r.file.Close()
+	}
+
+	portaudio.Terminate()
+}
+
+// WAV Header function --> writeWAVHeader
+
+func writeWAVHeader(f *os.File) error {
+	header := make([]byte, 44)
+	copy(header[:4], "RIFF")
+	copy(header[8:12], "WAVE")
+	copy(header[12:16], "fmt ")
+	binary.LittleEndian.PutUint32(header[16:20], 16) // PCM format
+	binary.LittleEndian.PutUint16(header[20:22], 1)  // Audio format (PCM)
+	binary.LittleEndian.PutUint16(header[22:24], Channels)
+	binary.LittleEndian.PutUint32(header[24:28], SampleRate)
+	binary.LittleEndian.PutUint32(header[28:32], SampleRate*Channels*(BitDepth/8))
+	binary.LittleEndian.PutUint16(header[32:34], Channels*(BitDepth/8))
+	binary.LittleEndian.PutUint16(header[34:36], BitDepth)
+	copy(header[36:40], "data")
+	_, err := f.Write(header)
+	return err
+}
+
+// update WAV Header with file BufferSize
+func updateWAVHeader(f *os.File) {
+	info, err := f.Stat()
+	if err != nil {
+		slog.Error("Failed to get file info: ", "error", err)
+		return
+	}
+	fileSize := uint32(info.Size())
+	dataSize := fileSize - 44
+
+	// Update RIFF chunk size
+	f.Seek(4, 0)
+	binary.Write(f, binary.LittleEndian, fileSize-8)
+
+	// Update data chunk size
+	f.Seek(40, 0)
+	binary.Write(f, binary.LittleEndian, dataSize)
 }
