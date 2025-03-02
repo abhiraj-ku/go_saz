b7aa7c743843ca4e18f2645fd3b931fdc5d5c6e8
Abhishek Kumar <abhirajabhi312@gmail.com>
2025-03-02 20:26:00 +0530

    feat: added function to init the spectogram generator

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/abhiraj-ku/go_saz.git
 
 go 1.24.0
 
-require github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b // indirect
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/go-audio/wav v1.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b // indirect
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
+	golang.org/x/image v0.14.0 // indirect
+	gonum.org/v1/gonum v0.15.1 // indirect
+)
---
+++ pkg/audio/spectogram.go
@@ -0,0 +1,59 @@
+package audio
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-audio/wav"
+)
+
+// Spectogram specifications (consts)
+const (
+	WindowSize    = 1024 // no of samples per fft Window
+	Overlap       = 512  // 50% overlap for smooth transition
+	FrequencyBins = WindowSize / 2
+)
+
+// Spectogram struct
+type Spectogram struct {
+	filepath   string
+	data       []float64
+	sampleRate int
+}
+
+// NewSpectogram initializes the Spectogram and reads wav file data
+
+func NewSpectogramGenerator(filepath string) (*Spectogram, error) {
+	// Open the WAV file and read PCM data (among the 44 bits of tha audio data)
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open desired file: %w", err)
+	}
+	defer file.Close()
+
+	// Using go-audio/wav read the wav file content
+	decoder := wav.NewDecoder(file)
+	if !decoder.IsValidFile() {
+		return nil, fmt.Errorf("provided file is not a .wav file: %w", err)
+	}
+	// read data
+	decoder.ReadInfo()
+	if decoder.SampleRate != SampleRate {
+		return nil, fmt.Errorf("unspported sample rate: %w", err)
+	}
+
+	// Read the raw PCM data
+	buf, err := decoder.FullPCMBuffer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read the PCM data: %w", err)
+	}
+
+	// convert PCM data to float64 (normalize it to [-1,1])
+	floatData := make([]float64, len(buf.Data))
+	for i, sample := range buf.Data {
+		floatData[i] = float64(sample) / float64(1<<15)
+	}
+
+	return &Spectogram{filepath, floatData, int(decoder.SampleRate)}, nil
+
+}
